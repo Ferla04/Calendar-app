ae23cae46f36e333f6f4d179278db7c9ce8e909f
Ferla04 <fer.velas135@gmail.com>
2022-10-19 13:45:56 -0500

    creacion de la rutas y navbar

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,21 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { LoginPage } from '../auth'
+import { CalendarPages } from '../calendar'
+
+
+export const AppRouter = () => {
+
+  const authStatus = 'authenticated'//'not-authenticated'
+
+  return (
+    <Routes>
+      {
+        ( authStatus === 'not-authenticated' ) 
+        ? <Route path='/auth/*' element={<LoginPage />}/>
+        : <Route path='/*' element={<CalendarPages />}/>
+      }
+
+      <Route path='/*' element={ <Navigate to='/auth/login'/> }/>
+    </Routes>
+  )
+}
