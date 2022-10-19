6b369bc7c2beef022f7ec36a6b400842ee3c15bf
Ferla04 <fer.velas135@gmail.com>
2022-10-19 15:16:05 -0500

    diseño e idioma del calendario

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useLayoutEffect, useRef } from 'react'
 
-export const Navbar = () => {
+export const Navbar = ({ setHeightNavbar }) => {
+  
+  const navH = useRef()
+
+  useLayoutEffect(() => {
+    setHeightNavbar( navH.current.clientHeight + 24 )
+  }, [])
+
+
   return (
-    <div className='navbar navbar-dark bg-dark mb-4 px-4'>
+    <div className='navbar navbar-dark bg-dark mb-4 px-4' ref={ navH }>
       <span className='navbar-brand'>
         <i className='fas fa-calendar-alt'></i>
         &nbsp;
--- src/calendar/pages/CalendarPages.jsx
+++ src/calendar/pages/CalendarPages.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { useState } from 'react'
+import { Calendar } from 'react-big-calendar'
+import { addHours } from 'date-fns'
 import { Navbar } from '../'
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { localizer, getMessagesES } from '../../helpers'
+
+
+const events = [{
+  title: 'Navidad',
+  notes: 'Vomprar regalos',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Fernanda'
+  }
+}]
 
 
 export const CalendarPages = () => {
+  const [ heightNavbar, setHeightNavbar ] = useState(0)
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    console.log({ event, start, end, isSelected })
+  }
+
   return (
     <>
-      <Navbar/>
+      <Navbar setHeightNavbar={ setHeightNavbar }/>
+
+      <Calendar
+        culture='es'
+        messages={ getMessagesES() }
+        localizer={ localizer }
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: `calc( 100vh - ${heightNavbar}px )` }}
+        eventPropGetter={ eventStyleGetter }
+      />
     </>
   )
 }
